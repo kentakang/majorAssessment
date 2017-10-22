55a74d30fa03e0a58616bb302c06d31250b78003
Chan Kang <kentakang@icloud.com>
2017-10-23 07:26:59 +0900

    Modfiy baskkinRobbins31.c

--- baskinRobbins31.c
+++ baskinRobbins31.c
@@ -25,7 +25,7 @@ int main()
 			}
 			printf("\n");
 			
-			if(cnt == 31)
+			if(cnt >= 31)
 			{
 				printf("computer win");
 				break;
@@ -43,7 +43,7 @@ int main()
 			}
 			printf("\n");
 			
-			if(cnt == 31)
+			if(cnt >= 31)
 			{
 				printf("you win");
 				break;
